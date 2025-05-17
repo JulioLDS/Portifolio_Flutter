e1ca98e1699f64a936c9707cbee5fbb68b857de1
Julio <juliolima1401@gmail.com>
2025-05-16 21:33:25 -0300

    Deixando a página Responsiva - Muda para a versão mobile se a largura da tela for menor que 600px

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:meu_portifolio/constants/colors.dart';
+import 'package:meu_portifolio/constants/size.dart';
 import 'package:meu_portifolio/widgets/drawer_mobile.dart';
+import 'package:meu_portifolio/widgets/header_desktop.dart';
 import 'package:meu_portifolio/widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,42 +17,48 @@ class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: CustomColor.scaffoldBg,
-      endDrawer: const DrawerMobile(),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          //MAIN
-          // HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: () {},
-            onMenuTap: () {
-              scaffoldKey.currentState?.openEndDrawer();
-            },
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: CustomColor.scaffoldBg,
+          endDrawer: constraints.maxWidth>=kMinDesktopWidth?null: const DrawerMobile(),
+          body: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              //MAIN
+              if(constraints.maxWidth>=kMinDesktopWidth)
+                const HeaderDesktop()
+              else
+                HeaderMobile(
+                  onLogoTap: () {},
+                  onMenuTap: () {
+                    scaffoldKey.currentState?.openEndDrawer();
+                  },
+                ),
+              //Skills
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+        
+              //Projects
+              Container(height: 500, width: double.maxFinite),
+        
+              //Contact
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+        
+              //Footer
+              Container(height: 500, width: double.maxFinite),
+            ],
           ),
-          //Skills
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-
-          //Projects
-          Container(height: 500, width: double.maxFinite),
-
-          //Contact
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-
-          //Footer
-          Container(height: 500, width: double.maxFinite),
-        ],
-      ),
+        );
+      }
     );
   }
 }
